32bafabc0297ae5d7ef5db6e387977614ac96316
yeseul-kim01 <yesul0718@naver.com>
2024-07-29 15:38:23 +0900

    work

---
+++ src/main/java/com/example/sapp4/Sapp4Application.java
@@ -0,0 +1,13 @@
+package com.example.sapp4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sapp4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sapp4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sapp4/Sapp4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sapp4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sapp4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
